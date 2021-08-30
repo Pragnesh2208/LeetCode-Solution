6064f4d21832e8ef40ddfd3047e9eed16a5fea38
Pragnesh2208 <pragnesh.amadavadi07@gmail.com>
2021-08-30 15:46:09 +0530

    #53 kadan's algorithm
    Algorithm to find largest sum of subarray

---
+++ Array/#53 Longest sum of subarray.cpp
@@ -0,0 +1,19 @@
+/******************** Only approach To find largest sum of subarray****************/
+/*******************Time complexity = O(N) **************************************************/
+/*******************Space complexity = O(1)  ***********************************************/
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int max=INT_MIN,sum=0;
+        for(auto i:nums)
+        {
+            sum+=i;
+            if(sum>max)
+                max=sum;
+            if(sum<0)
+                sum=0;
+        }
+        return max;
+        
+    }
+};
