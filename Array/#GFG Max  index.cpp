a8ff95a628628c2f0b98a4f894603ac2fe862514
Pragnesh2208 <pragnesh.amadavadi07@gmail.com>
2021-09-05 12:33:22 +0530

    Create #GFG Max  index.cpp
    Added brute Force and optimal approach

---
+++ Array/#GFG Max  index.cpp
@@ -0,0 +1,49 @@
+/*******************************Optimal Approach to Find Max Index*************************************************************/
+/*******************************Time complexity = O(N)*************************************************************************/
+/*******************************Space complexity = O(N)************************************************************************/
+class Solution{
+    public:
+            int maxIndexDiff(int A[], int n) 
+                { 
+                    int Min[n],maxdist=INT_MIN;
+                    Min[0]=A[0];
+                    for(int i=1;i<n;i++)
+                    {
+                        Min[i]=min(A[i],Min[i-1]);
+                    }
+                    int i=n-1,j=n-1;
+                    while(i>=0 && j>=0)
+                    {
+                        if(A[j]>=Min[i])
+                        {
+                            maxdist=max(j-i , maxdist);
+                            i--;
+                        }
+                        else
+                        {
+                            j--;
+                        }
+                    }
+                    return maxdist;
+                }
+};
+
+/*******************************Brute Force Approach to Find Max Index*************************************************************/
+/*******************************Time complexity = O(N^2) Big o of N square********************************************************/
+/*******************************Space complexity = O(N)**************************************************************************/
+
+class Solution{
+    public:
+            int maxIndexDiff(int A[], int n) 
+                { 
+                    int maxdist=INT_MIN;
+                    for(int i=0;i<n;i++)
+                    {
+                        for(int j=0;j<n;j++)
+                        {
+                            if(A[j]>=A[i])
+                            maxdist=max(maxdist,j-i);
+                        }
+                    }
+                }
+};
