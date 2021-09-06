1fe6c487371a7dd8659507bfd504cd58df3be1e3
Pragnesh2208 <pragnesh.amadavadi07@gmail.com>
2021-09-06 11:38:16 +0530

    Create #56 Merge Intervals.cpp
    Added Optimal approach

---
+++ Array/#56 Merge Intervals.cpp
@@ -0,0 +1,24 @@
+/******************** Optimal approach To find Merge overlapping intervals of  array****************/
+/*******************Time complexity = O(N) *********************************************************/
+/*******************Space complexity = O(1)*********************************************************/
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>>ans;
+        sort(intervals.begin() ,intervals.end());
+        vector<int>temp=intervals[0];
+        for(auto i : intervals)
+        {
+            if(temp[1]>=i[0])
+                temp[1]=max(i[1],temp[1]);
+            else
+            {
+                ans.push_back(temp);
+            temp=i;
+            }
+        }
+        ans.push_back(temp);
+        return ans;
+    }
+};
