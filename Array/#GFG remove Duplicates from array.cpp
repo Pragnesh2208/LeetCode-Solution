fe4813a1b6c6ac0f3dcb1ebacd832fcc38fb8851
Pragnesh2208 <pragnesh.amadavadi07@gmail.com>
2021-09-08 13:35:48 +0530

    Update #GFG remove Duplicates from array.cpp

--- Array/#GFG remove Duplicates from array.cpp
+++ Array/#GFG remove Duplicates from array.cpp
@@ -1,4 +1,4 @@
-/******************** Optimal approach To Remove duplicates from array************************/
+/******************** Brute Force approach To Remove duplicates from array************************/
 /*******************Time complexity = O(N)****************************************************/
 /*******************Space complexity = O(N)***************************************************/
 class Solution {
@@ -18,3 +18,27 @@ public:
     return ans;
     }
 };
+/******************** Optimal approach To Remove duplicates from array************************/
+/*******************Time complexity = O(N)****************************************************/
+/*******************Space complexity = O(1)***************************************************/
+
+class Solution{
+    private:
+vector<int> removeDuplicate(vector<int>& arr, int n)
+    {  long  prod=arr[0];
+      long  index=1;
+      for(int i=1;i<n;i++)
+      {
+          if(prod%arr[i]!=0)
+          {
+              arr[index]=arr[i];
+            
+              prod*=arr[index];
+              
+                index++;
+          }     
+      }
+      arr.erase(arr.begin()+index,arr.end());
+      return arr;
+    }
+}
