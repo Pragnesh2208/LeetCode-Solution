35c35e67618fb6b00d6a4ee422b059b2991231bf
Pragnesh2208 <pragnesh.amadavadi07@gmail.com>
2021-09-19 01:02:07 +0530

    Update #189 Rotate Array by K steps.cpp

--- Array/#189 Rotate Array by K steps.cpp
+++ Array/#189 Rotate Array by K steps.cpp
@@ -4,21 +4,17 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-      
         int n=nums.size();
-         k%=n;
-          int i=0,j=n-1;
-        if(k==0)
-        return;
-        while(i<j)
-            swap(nums[i++],nums[j--]);
-        i=0;j=k-1;
-        while(i<j)
-            swap(nums[i++],nums[j--]);
-        i=k,j=n-1;
-        while(i<j)
-            swap(nums[i++],nums[j--]);
-    
-        
+        k=k%n;
+        reverse(nums , 0 , n-1);
+        reverse(nums , 0 , k-1);
+        reverse(nums , k ,n-1);
+    }
+    void reverse(vector<int>&nums , int start , int end)
+    {
+        while(start < end)
+        {
+            swap(nums[start++] ,nums[end--]);
+        }
     }
 };
