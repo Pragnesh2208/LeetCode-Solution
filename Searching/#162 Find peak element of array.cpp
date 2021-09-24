4cc5d4b89a8f02306e5b350ba2463ff63ccb9dd6
Pragnesh2208 <pragnesh.amadavadi07@gmail.com>
2021-09-25 01:58:30 +0530

    Create #162 Find peak element of array.cpp

---
+++ Searching/#162 Find peak element of array.cpp
@@ -0,0 +1,19 @@
+/******************************Optimal approach To Find peak element in array*************************/
+/*****************************Time complexity = O(LOGN)***********************************************/
+/*****************************Space complexity = O(1)*************************************************/
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int left=0,right=nums.size()-1;
+        while(left<right)
+        {
+            int mid=left+(right-left)/2;
+            if(nums[mid]>nums[mid+1])
+                right=mid;
+                else
+                    left=mid+1;
+        }
+        return left;
+        
+    }
+};
