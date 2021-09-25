fc82b0b2c53c3e587e5dfaa2cdca0dc64d8aa57f
Pragnesh2208 <pragnesh.amadavadi07@gmail.com>
2021-09-26 02:57:47 +0530

    Update #153 Find minimum in rotate sorted array.cpp

--- Searching/#153 Find minimum in rotate sorted array.cpp
+++ Searching/#153 Find minimum in rotate sorted array.cpp
@@ -23,3 +23,25 @@ public:
         return ans;
     }
 };
+/******************************Optimal approach To Find minimum in rotate sorted array**********************/
+/******************************Time complexity = O(LOGN)***********************************************/
+/******************************Space complexity = O(1)*************************************************/
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int left=0,right=nums.size()-1;
+        while(left < right)
+        {
+            int mid=left+(right-left)/2;
+           if(nums[mid] < nums[right])
+           {
+               right=mid;
+           }
+            else{
+                left=mid+1;
+            }
+        }
+        return nums[right];
+        
+    }
+};
