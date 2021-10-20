91ecd19cb6e111484888577c243cd87a3c856f22
Pragnesh2208 <pragnesh.amadavadi07@gmail.com>
2021-10-20 09:21:08 +0530

    Create #503 Next Greater Element II.cpp

---
+++ Stack/#503 Next Greater Element II.cpp
@@ -0,0 +1,19 @@
+/************************************Optimal solution to find next greater element******/
+/************************************Time complexity = O( N) *****************/
+/************************************Space Complexity = O ( N )************************/
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        stack<int>st;
+        int n = nums.size();
+        vector<int>answer(n);
+        for(int i= 2*n-1 ; i>=0 ; i--) {
+        while(!st.empty() && nums[st.top()] <= nums[i%n]) {
+            st.pop();   
+        }
+            answer[i%n] = st.empty() ? -1 : nums[st.top()];
+            st.push(i%n);
+      }
+        return answer;
+    }
+};
