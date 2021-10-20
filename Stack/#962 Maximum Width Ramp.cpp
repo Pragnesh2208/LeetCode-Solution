437d65452f4c179a001e80ef5a1051e7e2675c9f
Pragnesh2208 <pragnesh.amadavadi07@gmail.com>
2021-10-21 00:53:49 +0530

    Create #962 Maximum Width Ramp.cpp

---
+++ Stack/#962 Maximum Width Ramp.cpp
@@ -0,0 +1,24 @@
+/************************************Optimal approach to find next greater element*****/
+/************************************Time complexity = O( N)***************************/
+/************************************Space Complexity = O ( N )************************/
+class Solution {
+public:
+    int maxWidthRamp(vector<int>& nums) {
+        stack<int>st;
+        int ans = 0;
+        int n = nums.size();
+        for(int i = 0 ; i < n ; i++ ) {
+            if(st.empty() || nums[st.top()] >= nums[i]) {
+                st.push(i);
+            }
+        }
+        for(int i = n-1 ; i >= 0 ; i-- ) {
+            while(!st.empty() && nums [ st.top() ] <= nums[i])
+            {
+                ans = max(ans , i - st.top());
+                st.pop();
+            }
+        }
+        return ans;
+    }
+};
