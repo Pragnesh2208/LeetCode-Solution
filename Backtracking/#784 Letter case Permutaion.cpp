d32751f28b1c8f758e0ba499b010fb9ae404f47b
Pragnesh2208 <pragnesh.amadavadi07@gmail.com>
2021-12-08 15:39:07 +0530

    Create #784 Letter case Permutaion.cpp

---
+++ Backtracking/#784 Letter case Permutaion.cpp
@@ -0,0 +1,26 @@
+/************************************Optimal solution to Generating Palindrome Partition********************************/
+/************************************Time Complexity is O(2^N) ****************************************************/
+/************************************Space Complexity is O( N )*****************************************************/
+class Solution {
+    private:
+    void getPermutation(vector<string> &str , string s, int ind) {
+        int n = s.length();
+        if(ind == n) {
+            str.push_back(s);
+            return;
+        }
+       if(isalpha(s[ind])) {
+           s[ind] = char(toupper(s[ind]));
+           getPermutation(str , s , ind +1);
+           s[ind] = char(tolower(s[ind]));
+       }
+        getPermutation(str , s , ind +1);
+    }
+    
+public:
+    vector<string> letterCasePermutation(string s) {
+        vector<string>str;
+        getPermutation(str,s,0);
+        return str;
+    }
+};
