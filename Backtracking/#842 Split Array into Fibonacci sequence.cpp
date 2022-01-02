e7708d97e4aa4e0ead011b10b9823a6d7a404991
Pragnesh2208 <pragnesh.amadavadi07@gmail.com>
2022-01-02 16:20:02 +0530

    Update #842 Split Array into Fibonacci sequence.cpp

--- Backtracking/#842 Split Array into Fibonacci sequence.cpp
+++ Backtracking/#842 Split Array into Fibonacci sequence.cpp
@@ -1,3 +1,7 @@
+/************************************Optimal approach to Split Array into Fibonacci Sequence***********************************/ 
+/**********************************************Time Complexity  = O (10 ^ N)*************************************************/
+/*********************************************At each level, we branch at most 10 times, as the for-loop will run until Integer.MAX_VALUE is reached (which is maximum 10 digits in length).Our recursive calls go to a maximum depth of       N********************************************************************/
+/**********************************************Space Complexity  = O (N) which is the Depth of Recursion Tree**************************************************/
 class Solution {
     private:
     bool getCombination(int ind , string st , vector<int> &ans) {
@@ -28,7 +32,6 @@ class Solution {
              
                 ans.push_back((int)num);
                 if(getCombination(i + 1 , st , ans) == true) { 
-                    cout<<"hello"<<endl;
                     return true;
                 }
                     ans.pop_back();
