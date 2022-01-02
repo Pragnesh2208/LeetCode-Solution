5920a056298d6a81e507b87bd5734df1f6e63767
Pragnesh2208 <pragnesh.amadavadi07@gmail.com>
2022-01-02 16:01:54 +0530

    Create #842 Split Array into Fibonacci sequence.cpp

---
+++ Backtracking/#842 Split Array into Fibonacci sequence.cpp
@@ -0,0 +1,45 @@
+class Solution {
+    private:
+    bool getCombination(int ind , string st , vector<int> &ans) {
+        if(ind == st.size() ) {
+             if(ans.size()>=3)
+                 return true;
+            else
+                return false;
+        }
+        
+        int n = st.size();
+        long num = 0;
+        for(int i = ind ; i < n ; i++ ) {
+            if(i > ind && st[ind] == '0')
+                return false;
+            
+             num = num*10 + (st[i] - '0');
+            
+            if (num > INT_MAX) 
+              break;
+        
+            int size = ans.size();
+            
+            if(size >= 2 && num - ans[size - 1] - ans[size - 2]  > 0) 
+                break;
+            
+         if( size <= 1 || num - ans[size - 1] - ans[size - 2] == 0 ) {
+             
+                ans.push_back((int)num);
+                if(getCombination(i + 1 , st , ans) == true) { 
+                    cout<<"hello"<<endl;
+                    return true;
+                }
+                    ans.pop_back();
+            }
+        }
+        return false;
+    }
+public:
+    vector<int> splitIntoFibonacci(string num) {
+        vector<int> ans;
+        getCombination( 0 , num , ans );
+        return ans;
+    }
+};
