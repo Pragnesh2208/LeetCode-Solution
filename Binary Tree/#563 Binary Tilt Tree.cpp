ce8475ae5bfe7d13a7d60701ab6b673f2752b40a
Pragnesh2208 <pragnesh.amadavadi07@gmail.com>
2022-02-02 13:05:15 +0530

    Create #563 Binary Tilt Tree.cpp

---
+++ Binary Tree/#563 Binary Tilt Tree.cpp
@@ -0,0 +1,30 @@
+/**************************Optimal Approach for Binary Tilt Tree*******************************************/
+/**************************Time Complexity = O(N)************************************************************/
+/*************************Space Complexity = O(H)************************************************************/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    int Sum(TreeNode * root , int &sum) {
+        if(root == NULL)
+            return 0;
+      int  left = Sum(root -> left , sum);
+      int right = Sum(root -> right , sum);
+        sum +=abs(left - right);
+        return (left + right+ root -> val);
+    }
+public:
+    int findTilt(TreeNode* root) {
+        int sum = 0;
+        int x = Sum(root , sum);
+        return sum;
+    }
+};
