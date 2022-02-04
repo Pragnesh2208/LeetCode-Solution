a7ac47464d4ec2b37d7c58463894bf0e9b872370
Pragnesh2208 <pragnesh.amadavadi07@gmail.com>
2022-02-04 21:07:44 +0530

    Create #783 . Minimum Distance Between BST Nodes.cpp

---
+++ Binary Tree/#783 . Minimum Distance Between BST Nodes.cpp
@@ -0,0 +1,77 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+/***********************Recurive Brute Force Approach to Find Minimum Difference in BST******************/
+/************************Time Complexity = O(N)********************************************************/
+/************************Space Complexity = O(H)******************************************************/
+class Solution {
+     private :
+    int prev = -1;
+    private:
+    void inorder(TreeNode * root , int &ans) {
+      if(root -> left) inorder(root -> left , ans);
+        if(prev!= -1) {
+            ans = min(ans , root -> val - prev);
+        
+        }
+        prev = root -> val;
+      if(root -> right) inorder(root -> right , ans);
+    }
+public:
+    int minDiffInBST(TreeNode* root) {
+     int ans = INT_MAX;
+        inorder(root , ans);
+        return ans;   
+    }
+};
+/*****************Optimal Approach to Find Minimum Difference in BST using Morris Traversal******************/
+/******************************Time Complexity = O(N)********************************************************/
+/*******************************Space Complexity = O(1)******************************************************/
+class Solution {
+    private :
+    int prev = -1;
+    private:
+    void checkPrev(int &prev, int &ans , TreeNode * &root ) {
+         if(prev != -1){
+                 ans = min(ans , root -> val - prev);
+             }
+             prev = root -> val;
+    }
+    void inorder(TreeNode * root , int &ans) {
+     while(root){
+         if(root -> left == NULL) {
+            checkPrev(prev , ans , root);
+             root = root -> right;
+         }
+         else {
+             TreeNode * pre = root -> left;
+             while(pre -> right && pre -> right != root) {
+                 pre = pre -> right;
+             }
+             if(pre -> right == root) {
+                 pre -> right = NULL;
+                 checkPrev(prev , ans ,root);
+                 root = root -> right;
+             }
+             else {
+                 pre -> right = root;
+                 root = root -> left;
+             }
+         }
+     }
+    }
+public:
+    int minDiffInBST(TreeNode* root) {
+     int ans = INT_MAX;
+        inorder(root , ans);
+        return ans;   
+    }
+};
