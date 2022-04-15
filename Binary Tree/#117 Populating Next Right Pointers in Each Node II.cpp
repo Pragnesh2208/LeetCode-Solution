fe0ac51d051195f541e92f45fb9e66224ee1b47e
Pragnesh2208 <pragnesh.amadavadi07@gmail.com>
2022-04-15 20:49:37 +0530

    Create #117 Populating Next Right Pointers in Each Node II.cpp

---
+++ Binary Tree/#117 Populating Next Right Pointers in Each Node II.cpp
@@ -0,0 +1,62 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+/***********************Optimal Approach Populating Next Right Pointers in Each Node II****/
+/************************Time Complexity = O(N)****************************************/
+/************************Space Complexity = O(N)***************************************/
+
+class Solution
+{
+public:
+    Node *connect(Node *root)
+    {
+        if (root == NULL)
+            return root;
+        Node *leftmost = root;
+        while (leftmost)
+        {
+
+            Node *cur = leftmost;
+            leftmost = NULL;
+            Node *prev = NULL;
+
+            while (cur)
+            {
+                if (cur->left)
+                {
+                    if (leftmost == NULL)
+                        leftmost = cur->left;
+                    if (prev != NULL)
+                        prev->next = cur->left;
+                    prev = cur->left;
+                }
+
+                if (cur->right)
+                {
+                    if (leftmost == NULL)
+                        leftmost = cur->right;
+                    if (prev != NULL)
+                        prev->next = cur->right;
+                    prev = cur->right;
+                }
+                cur = cur->next;
+            }
+        }
+        return root;
+    }
+};
