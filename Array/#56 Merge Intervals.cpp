b11273bffab8514cade316da0d19962947adbb7c
Pragnesh Amadavadi <pragnesh.amadavadi07@gmail.com>
2022-07-23 10:29:30 +0530

    Update #56 Merge Intervals.cpp

--- Array/#56 Merge Intervals.cpp
+++ Array/#56 Merge Intervals.cpp
@@ -2,23 +2,26 @@
 /*******************Time complexity = O(NlogN) *********************************************************/
 /*******************Space complexity = O(1)*********************************************************/
 
-class Solution {
+
+class Solution { 
+   
 public:
+    
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        
         vector<vector<int>>ans;
-        sort(intervals.begin() ,intervals.end());
-        vector<int>temp=intervals[0];
-        for(auto i : intervals)
-        {
-            if(temp[1]>=i[0])
-                temp[1]=max(i[1],temp[1]);
-            else
-            {
-                ans.push_back(temp);
-            temp=i;
+        sort(intervals.begin() , intervals.end() );
+        int n = intervals.size();
+        
+        for(int i = 0 ; i < n; i++){
+            
+            if(ans.size() != 0 && ans[ans.size() - 1][1] >= intervals[i][0] ) {
+                if(intervals[i][1] > ans[ans.size() - 1][1])
+                ans[ans.size() - 1][1] = intervals[i][1];
             }
+            else
+            ans.push_back(intervals[i]);
         }
-        ans.push_back(temp);
         return ans;
     }
 };
