46fb3359fd3514641603d67d35aa2a4a0f49b856
Pragnesh Amadavadi <pragnesh.amadavadi07@gmail.com>
2023-07-30 10:45:46 +0530

    Longest Common Supersequence added

---
+++ Dynamic Programming/#1092 Longest_Common_Supersequence.cpp
@@ -0,0 +1,56 @@
+/*
+Time Complexity = O(N*M)
+Space Complexity = O(N*M)
+*/
+class Solution {
+public:
+    string shortestCommonSupersequence(string str1, string str2) {
+        int n = str1.size() , m  = str2.size();
+
+        vector<vector<int>>dp(n + 1 , vector<int>(m + 1 , 0));
+        for(int i = 1 ; i <= n ; i++) {
+            for(int j = 1 ; j <= m ; j++) {
+                if(str1[i-1] == str2[j-1]) {
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                } else dp[i][j] = max(dp[i-1][j] , dp[i][j-1]);
+            }
+        }
+
+        string lcs = "";
+        int i = n , j = m;
+        while(i >= 1 && j >= 1) {
+           
+            if(str1[i - 1] == str2[j - 1]) {
+                lcs += str1[i-1];
+                
+                if(dp[i][j] == 0) {
+                    i--;
+                } else {
+                    i--;
+                    j--;
+                }
+            } else {
+                if(dp[i-1][j] > dp[i][j-1]) {
+                    lcs += str1[i - 1];
+                    i--;
+                } else{
+                    lcs += str2[j - 1];
+                    j--;
+                }
+            }
+        }
+
+        while(i >= 1) {
+            lcs += str1[i-1];
+            i--;
+        }
+
+        while(j >= 1) {
+            lcs += str2[j-1];
+            j--;
+        }
+        i = 0 , j = lcs.length() -1;
+        while(i < j) swap(lcs[i++] , lcs[j--]);
+        return lcs;
+    }
+};
