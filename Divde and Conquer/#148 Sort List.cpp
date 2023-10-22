6d1bbe5a344d344dcfe0205274076f19b13db545
Pragnesh Amadavadi <pragnesh.amadavadi07@gmail.com>
2023-10-22 15:41:14 +0530

    Clean Code added for Sort list

--- Divde and Conquer/#148 Sort List.cpp
+++ Divde and Conquer/#148 Sort List.cpp
@@ -81,3 +81,68 @@ public:
      return mergesortList(head);   
     }
 };
+
+
+/*
+Clean Code
+*/
+// } Driver Code Ends
+/* Structure of the linked list node is as
+struct Node 
+{
+    int data;
+    struct Node* next;
+    Node(int x) { data = x;  next = NULL; }
+};
+*/
+class Solution{
+  public:
+    Node * getMid(Node * head) {
+        if(!head) return NULL;
+        Node * slow = head , * fast = head;
+        while(fast -> next && fast -> next -> next) {
+            fast = fast -> next -> next;
+            slow = slow -> next;
+        }
+        return slow;
+    }
+    
+    Node * mergeTwoLists(Node * first , Node * second) {
+        if(!first) return second;
+        if(!second) return first;
+        Node * dummyHead = new Node(0);
+        Node * tail = dummyHead;
+        while(first && second) {
+            if(first -> data < second -> data) {
+                tail -> next = first;
+                first = first -> next;
+            }else {
+                tail -> next = second;
+                second = second -> next;
+            }
+            tail = tail -> next;
+        }
+        
+        if(first) tail -> next = first;
+        if(second) tail -> next = second;
+        return dummyHead -> next;
+    }
+    //Function to sort the given linked list using Merge Sort.
+    Node* mergeSort(Node* head) {
+        // your code here
+        if(!head || head -> next == NULL) return head;
+        Node * mid = getMid(head);
+        
+        if(mid) {
+            Node * temp = mid -> next;
+            mid  -> next = NULL;
+            mid = temp;      
+        }
+        
+        
+        Node * left = mergeSort(head);
+        Node * right = mergeSort(mid);
+        return mergeTwoLists(left , right);
+        
+    }
+};
