d1eb4c508a8311b997c3df2dddb9208f2d8440fb
Pragnesh Amadavadi <pragnesh.amadavadi07@gmail.com>
2023-10-28 11:01:03 +0530

    LRU Cache implementation updated

--- Linked List/#146 LRU Cache.cpp
+++ Linked List/#146 LRU Cache.cpp
@@ -1,5 +1,5 @@
 /*
-OPtimal solution for LRU Cache
+OPtimal solution for LRU Cache with delete node operation
 Average Time Complexity for get and put is O(1)
 Space Complexity = O(N)
 */
@@ -64,6 +64,98 @@ public:
               insert(key , val);
           }
       }
+    }
+};
+
+/*
+OPtimal solution for LRU Cache without delete node operation
+Average Time Complexity for get and put is O(1)
+Space Complexity = O(N)
+*/
+class customListNode {
+    public:
+    int val;
+    int key;
+    customListNode * next;
+    customListNode * prev;
+    customListNode(int value , int key) {
+        val = value;
+        this -> key = key;
+        next = nullptr;
+        prev = nullptr;
+    }
+};
+class LRUCache {
+    int capacity;
+    customListNode * head = new customListNode(-1 , -1), * tail = new customListNode(-1 , -1);
+    map<int,customListNode *>bucket;
+public:
+    LRUCache(int capacity) {
+        this->capacity = capacity;
+        head -> next = tail;
+        tail -> prev = head;
+    }
+
+    void removeLastNode(){
+        customListNode * node = tail -> prev;
+        tail -> prev  = node -> prev;
+        node -> prev -> next = tail;
+        deleteNode(node);
+    }
+
+    void deleteNode(customListNode * node) {
+        bucket.erase(node -> key);
+        delete(node);
+        return;
+    }
+
+    void removeFromList(customListNode *  node) {
+        node -> prev -> next = node -> next;
+        node -> next -> prev = node -> prev;
+        node -> next = nullptr;
+        node -> prev = nullptr;
+        return;
+    }
+
+    void push_front(customListNode * node) {
+        customListNode * root = head -> next;
+        head -> next = node;
+        node -> prev = head;
+        node -> next = root;
+        root -> prev = node;
+        return;
+    }
+
+    void insertNode(int key , int val , bool alreadyExist = false ) {
+        customListNode * temp;
+        if(alreadyExist) {
+            temp = bucket[key];
+            temp -> val = val;
+            removeFromList(temp);
+        }
+        else {
+            temp = new customListNode(val , key);
+        }
+        push_front(temp);
+        bucket[key] = temp;   
+    }
+
+    int get(int key) {
+        if(bucket.find(key) != bucket.end()) {
+           customListNode * cur = bucket[key];
+           removeFromList(cur);
+           push_front(cur);
+           return cur -> val;
+        }
+        return -1;
+    }
+    
+    void put(int key, int val) {
+        bool alreadyExist = bucket.find(key) != bucket.end();
+        if(!alreadyExist && bucket.size() == capacity) {
+            removeLastNode();
+        }
+        insertNode(key , val , alreadyExist);                      
     }
 };
 
