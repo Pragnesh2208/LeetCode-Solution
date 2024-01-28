c2a541fed262142b3dcc6bf749f5722e717f2c6d
Pragnesh Amadavadi <pragnesh.amadavadi07@gmail.com>
2024-01-28 10:33:15 +0530

    Lemonade changes updates with clean code

--- Greedy/#860 Lemonade Change.cpp
+++ Greedy/#860 Lemonade Change.cpp
@@ -32,3 +32,38 @@ public:
         return flag;
     }
 };
+
+/********************************Optimal approach to Solve Lemonade Change Problem********/ 
+/********************************Time Complexity  = O (  N  )*****************************/
+/********************************Space Complexity  = O ( 1 )******************************/
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        bool ans = false;
+        int n = bills.size();
+        vector<int>mp(3 ,0);
+
+        for(int i = 0 ; i < n ; i++) {
+            if(bills[i] == 5){ 
+                mp[0]++;
+                }
+            else if(bills[i] == 10) {
+                mp[1]++;
+                mp[0]--;
+                if(mp[0] < 0) return false; 
+                }
+            else{ 
+                mp[2]++;
+                if(mp[1] > 0) {mp[1]--;
+                } else {
+                    mp[0] -= 2;
+                }
+                mp[0]--;
+
+                if(mp[0] < 0 || mp[1] < 0) return false;
+                }
+        }
+
+        return true;
+    }
+};
