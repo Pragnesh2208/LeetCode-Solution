d7cff7c0649b38bc790a959bda1ab33bb020dbe5
Pragnesh Amadavadi <pragnesh.amadavadi07@gmail.com>
2024-02-07 22:44:13 +0530

    Next Greater Node in Linked List added

--- Linked List/#1019 Next Greater Node in Linked List.cpp
+++ Linked List/#1019 Next Greater Node in Linked List.cpp
@@ -28,3 +28,56 @@ public:
   return ans;
     }
 };
+
+/********************Optimal approach to Find Next Greater Node in Linked List by traversing linkedlist******/
+/*******************Time complexity = O(N)*******************************************************************/
+/*******************Space complexity = O(N)******************************************************************/
+class Solution {
+    private:
+    ListNode* reverseList(ListNode *head) {
+        ListNode dh = ListNode(0);
+        dh.next = head;
+        while(head -> next) {
+            ListNode * currNext = head -> next;
+            ListNode * tempNext = currNext -> next;
+            currNext -> next = dh.next;
+            dh.next = currNext;
+            head -> next = tempNext;
+        }
+
+        return dh.next;
+    }
+
+    int getLength(ListNode * head) {
+        int count = 0 ;
+        while(head) {head = head -> next;
+        count++;};
+        return count;
+    }
+
+public:
+    vector<int> nextLargerNodes(ListNode* head) {
+        int len = getLength(head);
+        vector<int>ans(len , 0);
+        stack<int>st;
+        head = reverseList(head);
+        int index = len - 1;
+        while(head) {
+            while(st.size() > 0 && st.top() <= head -> val) {
+                st.pop();
+            }
+
+            if(st.size() == 0) {
+                ans[index] = 0;
+            } else {
+                ans[index] = st.top();
+            }
+
+            index--;
+            st.push(head -> val);
+            head = head -> next;
+        }
+
+        return ans;
+    }
+};
