5ca6933dd728607bbf5494181ed48c7d9230932d
Pragnesh Amadavadi <pragnesh.amadavadi07@gmail.com>
2024-03-10 14:14:55 +0530

    Asteroid Collision Added

--- Stack/#735 Asteroid Collisions.cpp
+++ Stack/#735 Asteroid Collisions.cpp
@@ -35,3 +35,28 @@ public:
         return ans;
     }
 };
+
+// Clean Code & Short Code
+
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& nums) {
+        int n = nums.size();
+        stack<int>st;
+        for(int i = 0 ; i < n ; i++) {
+            while(nums[i] < 0 && !st.empty() && st.top() > 0 && abs(nums[i]) > st.top()) {
+                st.pop();
+            }
+            if( !st.empty() && nums[i] < 0 &&  abs(nums[i]) == st.top()) st.pop();
+            else if(nums[i] > 0 || st.empty() || abs(nums[i]) > st.top() )
+            st.push(nums[i]);
+        }
+
+        vector<int>ans(st.size());
+        while(!st.empty()) {
+            ans[st.size() - 1] = st.top();
+            st.pop();
+        }
+        return ans;
+    }
+};
