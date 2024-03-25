19e106528faa894d0b6c1acc60020cf95e086b10
Pragnesh Amadavadi <pragnesh.amadavadi07@gmail.com>
2024-03-25 22:45:51 +0530

    Top K Frequent Element Of Array Updated

--- Divde and Conquer/#347 Top K Frequent Element of Array.cpp
+++ Divde and Conquer/#347 Top K Frequent Element of Array.cpp
@@ -1,4 +1,4 @@
-/************************Optimal solution to find Top K Frequent Element of Array*************************/
+/************************Optimal solution to find Top K Frequent Element of Array Using Divide And Conquer*************************/
 /************************Time Complexity = O(N)**************************************************/
 /************************Space Complexity = O(N)*************************************************/
 /****************************Refer video of NeetCode*********************************/
@@ -7,7 +7,7 @@ class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         unordered_map<int,int>count;
-        unordered_map<int,vector<int>>freq;
+        vector<int,vector<int>>freq(nums.size() + 1);
         vector<int>ans;
         int n = nums.size();
         for(int i = 0 ; i < n ; i++) count[nums[i]]++;
@@ -25,5 +25,44 @@ public:
             }
         }
     return ans;
+    }
+};
+
+/************************Better solution to find Top K Frequent Element of Array using Heap*************************/
+/************************Time Complexity = O(NLOGK)**************************************************/
+/************************Space Complexity = O(N)*************************************************/
+
+class myComp{
+    public:
+    bool operator()(pair<int,int> a , pair<int,int>b) {
+        return a.second > b.second;
+    }
+};
+
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        priority_queue<pair<int,int> , vector<pair<int,int>> , myComp>minHeap;
+        int n = nums.size();
+        unordered_map<int,int>bucket;
+        vector<int> ans;
+
+        for(int i = 0 ; i < n ;i++) {
+            bucket[nums[i]]++;
+        }
+
+        for(auto p : bucket) {
+            minHeap.push({p.first , p.second});
+            if(minHeap.size() > k) minHeap.pop();
+        }
+
+        while(!minHeap.empty()) {
+            int topEle = minHeap.top().first;
+
+            minHeap.pop();
+            ans.push_back(topEle);
+        }
+
+        return ans;
     }
 };
