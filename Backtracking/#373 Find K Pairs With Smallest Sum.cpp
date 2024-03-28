a993273a147da53e51e55414965989aff43210f6
Pragnesh Amadavadi <pragnesh.amadavadi07@gmail.com>
2024-03-28 22:14:16 +0530

    Find K Pair with smallest sum updated

--- Backtracking/#373 Find K Pairs With Smallest Sum.cpp
+++ Backtracking/#373 Find K Pairs With Smallest Sum.cpp
@@ -4,48 +4,36 @@
 /************************************Space Complexity is O( K)*****************************************************/
 
 class Solution {
-    private :
-    class point{
-        public:
-        int num1,num2,sum , i , j;
-        point(int _num1 , int _num2 , int _i ,int _j) {
-            num1 = _num1;
-            num2 = _num2;
-            sum = num1 + num2;
-            i = _i;
-            j = _j;
-        }  
-    };
-    class comp{
-        public:
-        bool operator()(point &p1, point &p2) {
-            return p1.sum > p2.sum;      
-        }
-    };
-
 public:
     vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
-        
-        int n = nums1.size(), m = nums2.size();
-        priority_queue<point , vector<point>,comp>pq;
+        priority_queue<pair<int , pair<int,int>> , vector<pair<int,pair<int,int>>> , greater<pair<int , pair<int,int>>>>minHeap;
+        set<pair<int,int>>visited;
+        int n = nums1.size() , m = nums2.size();
         vector<vector<int>> ans;
-        if(n == 0 || m == 0 )
-            return ans;
-        for(int i = 0 ; i < n ;i++)
-            pq.push ( point( nums1[i] , nums2[0] , i , 0 ) );
-    
-        for(int i = 0 ; i < k && !pq.empty() ; i++)  {
-            
-            point temp = pq.top();
-            cout<<temp.num1<<" "<<temp.num2<<endl;
-            ans.push_back ( {temp.num1, temp.num2});
-            pq.pop();
-            if(temp.j + 1 == m)
-            continue;
-            pq.push(point  (temp.num1 , nums2[temp.j + 1] , temp.i , temp.j +1 ) );
-            
+
+        minHeap.push({nums1[0] + nums2[0] , {0 , 0}});
+
+        visited.insert({0 , 0});
+
+        while( k > 0 && !minHeap.empty()) {
+            pair<int , pair<int,int>> minEle = minHeap.top();
+            int firstEle = minEle.second.first;
+            int secondEle = minEle.second.second;
+            int fi = firstEle , si = secondEle;
+            ans.push_back({nums1[firstEle] , nums2[secondEle]});
+            minHeap.pop();
+            k--;
+            if(fi + 1 < n && visited.find({fi + 1 , si}) == visited.end()) {
+                minHeap.push({nums1[fi + 1] + nums2[si] , {fi + 1,  si}});
+                visited.insert({fi + 1, si});
+            }
+
+            if(si + 1 < m && visited.find({fi, si + 1}) == visited.end()) {
+                minHeap.push({nums1[fi] + nums2[si + 1] , {fi , si + 1}});
+                visited.insert({fi , si + 1});
+            }
         }
+
         return ans;
-        
-       }
+    }
 };
