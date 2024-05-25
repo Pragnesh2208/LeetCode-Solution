e03b9002419b95ade9e8d9ea6b5b410ff48c18ab
Pragnesh Amadavadi <pragnesh.amadavadi07@gmail.com>
2024-05-25 18:16:16 +0530

    Permutation II updated

--- Backtracking/#47 Permutation II.cpp
+++ Backtracking/#47 Permutation II.cpp
@@ -1,29 +1,34 @@
-/********************************Optimal approach to Print all Permutation*********/ 
+/********************************Optimal approach to Print all Unique Permutation*********/ 
 /********************************Time Complexity  = O (  N * N!  )************************/
 /********************************Here N to putting nums in result and N! for total permutations************/
 /********************************Space Complexity  = O ( N )******************************/
 class Solution {
-private:
-    void getPermutation(int ind , vector<vector<int>> &ans , vector<int> &arr) {
-         int N = arr.size();
-        if(ind == N){
-            ans.push_back(arr);
+    void getUniquePermutation( unordered_map<int,int>&freq , vector<vector<int>>&ans , vector<int>temp , int l) {
+        if(temp.size() == l) {
+            ans.push_back(temp);
             return;
         }
-      
-        for(int currIndex = ind ; currIndex < N ; currIndex++){
-             if(currIndex > ind && arr[currIndex] == arr[currIndex - 1])
-                 continue;
-            swap(arr[currIndex] , arr[ind]);
-            getPermutation(ind + 1 , ans , arr );
-            swap(arr[currIndex] , arr [ ind]);
+
+        for(pair<int,int>p : freq) {
+            int number = p.first , count = p.second;
+            if(count == 0) continue;
+            temp.push_back(number);
+            freq[number]--;
+            getUniquePermutation( freq , ans , temp ,l );
+            freq[number]++;
+            temp.pop_back();
         }
     }
 public:
     vector<vector<int>> permuteUnique(vector<int>& nums) {
         vector<vector<int>>ans;
-        sort(nums.begin() , nums.end());
-        getPermutation(0 ,ans , nums);
+        int n = nums.size();
+        vector<int>temp;
+        unordered_map<int,int>freq;
+        for(int i = 0; i < n ; i++) {
+            freq[nums[i]]++;
+        }
+        getUniquePermutation(freq , ans , temp , nums.size());
         return ans;
     }
 };
