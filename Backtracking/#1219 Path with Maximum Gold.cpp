b7935ba9a6793a60f7de39c391d16ec7fe63839e
Pragnesh Amadavadi <pragnesh.amadavadi07@gmail.com>
2024-06-22 18:30:47 +0530

    path with maximum gold code updated

--- Backtracking/#1219 Path with Maximum Gold.cpp
+++ Backtracking/#1219 Path with Maximum Gold.cpp
@@ -1,37 +1,35 @@
 /**********************************************Optimal approach to Path with Maximum Gold***********************************/ 
-/**********************************************Time Complexity  = O (4 ^ K)*************************************************/
-/**********************************************Here K is total cells conntains Gold and every cells has 4 Direction*********/
+/**********************************************Time Complexity  = O (N*M + 3^G)*************************************************/
+/**********************************************Here G is total cells conntains Gold and every cells has 3 Direction to explore*********/
 /**********************************************Space Complexity  = O ( K )**************************************************/
-class CombinationIterator {
 class Solution {
-    private:
-    void getPath(int row , int col , vector<vector<int>>&grid , int sum , int & ans) {
+    int getGold(int row , int col , vector<vector<int>>&grid) {
+        int totalRow = grid.size();
+        int totalCol = grid.back().size();
+        if(row < 0 || row >= totalRow || col < 0 || col >= totalCol || grid[row][col] == 0) return 0;
+        int gridValue = grid[row][col];
+        grid[row][col] = 0;
 
-         ans = max(sum , ans);
-
-        if( row < 0 || row == grid.size() || col < 0  || col == grid[row].size() || grid[row][col] == 0)
-            return;
-       
-        int right[] = {0 , 1 , 0 , -1};
-        int left[] = {-1 , 0 , 1 , 0};
-        for(int i = 0 ; i < 4 ; i++) {
-            sum += grid[row] [col];
-            int temp = grid[row][col];
-            grid[row][col] = 0;
-            getPath(row + left[i] , col + right[i] , grid , sum   , ans );
-            grid[row][col] = temp;
-            sum -= grid[row][col];
+        vector<pair<int,int>>directions = {{0 , -1} ,{ 0 , 1 },{ -1 , 0} ,{ 1 , 0}};
+        int maxDirection = 0;
+        for(int i = 0 ; i < directions.size() ; i++) {
+            maxDirection = max(maxDirection , getGold(row + directions[i].first , col + directions[i].second  , grid));
         }
+        grid[row][col] = gridValue;
+        return maxDirection + grid[row][col];
     }
 public:
     int getMaximumGold(vector<vector<int>>& grid) {
-        int ans = 0 , sum = 0;
-        for(int i = 0 ; i < grid.size() ; i++ ) {
-            for(int j = 0 ; j < grid[i].size() ; j++ ) {
-                if(grid[i][j] != 0) 
-                    getPath(i , j , grid , sum  , ans);
+        int totalRow = grid.size();
+        int totalCol = grid.back().size();
+
+        int ans = 0;
+        for(int i = 0 ; i < totalRow ; i++) {
+            for(int j = 0; j < totalCol ; j++) {
+               ans = max(ans , getGold(i , j  , grid ));
             }
         }
+
         return ans;
     }
 };
