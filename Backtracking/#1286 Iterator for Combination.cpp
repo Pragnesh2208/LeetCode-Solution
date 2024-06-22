94164dfbc311a6cf816b4b39314815b0faf25318
Pragnesh Amadavadi <pragnesh.amadavadi07@gmail.com>
2024-06-22 12:48:20 +0530

    iterator for combination updated

--- Backtracking/#1286 Iterator for Combination.cpp
+++ Backtracking/#1286 Iterator for Combination.cpp
@@ -1,4 +1,4 @@
-/********************************Optimal approach to Generator All Permutation*********/ 
+/********************************Brute force approach to Generator All Permutation*********/ 
 /********************************Time Complexity  = O (  C(N*K)* K  )************************/
 /********************************Here K to putting temp in ans and N * K for total permutations************/
 /********************************Space Complexity  = O ( K )******************************/
@@ -43,3 +43,53 @@ public:
  * string param_1 = obj->next();
  * bool param_2 = obj->hasNext();
  */
+
+/********************************Optimal approach to Generator All Permutation*********/ 
+/********************************Time Complexity  = O ( K  )************************/
+/********************************Space Complexity  = O ( K )******************************/
+class CombinationIterator {
+private:
+    vector<int> combination;
+    int k;
+    string str;
+    bool isNext = false;
+    
+    string getNextPermuation() {
+        string nextPermuation;
+        isNext = false;
+        for (int i = 0; i < k; i++) {
+            int index = combination[i];
+            nextPermuation.push_back(str[index]);  
+        }
+        for(int i =  k - 1 ; i >=  0; i--) {
+            if(combination[i] + 1 != combination[i + 1]) {
+                combination[i] = combination[i] + 1;
+                for(int j = i + 1 ; j < k ; j++) {
+                    combination[j] = combination[i] + j - i;
+                }
+                isNext = true;
+                break;
+            }
+        }
+        return nextPermuation;
+    }
+
+public:
+    CombinationIterator(string characters, int combinationLength) {
+        k = combinationLength;
+        str = characters;
+        for (int i = 0; i < k; i++) {
+            combination.push_back(i);
+        }
+        combination.push_back(str.length());
+        if(str.length()) {
+            isNext = true;
+        }
+    }
+
+    string next() { return getNextPermuation(); }
+
+    bool hasNext() {
+        return isNext;
+    }
+};
