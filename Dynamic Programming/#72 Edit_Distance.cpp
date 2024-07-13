aff1386d98993ae164e53e131b664e82594683cd
Pragnesh Amadavadi <pragnesh.amadavadi07@gmail.com>
2024-07-13 11:11:53 +0530

    edit distance updated

--- Dynamic Programming/#72 Edit_Distance.cpp
+++ Dynamic Programming/#72 Edit_Distance.cpp
@@ -1,4 +1,41 @@
 /*
+Clean Code
+Time Complexity = O(N*M)
+Space Complexity = O(N*M)
+Using Memoization 
+*/
+
+class Solution {
+    int solve(int ind1 , int ind2 , string &s1 , string &s2 , vector<vector<int>>&dp) {
+        int m = s2.length();
+        int n = s1.length();
+
+        if(ind2 == m) return n - ind1;
+
+        if(ind1 == n ) return m - ind2;
+
+        if(dp[ind1][ind2] != -1) return dp[ind1][ind2];
+
+        if(s1[ind1] == s2[ind2]) return dp[ind1][ind2] =  solve(ind1  + 1 , ind2 + 1 , s1 , s2 , dp);
+        
+        int insert = solve(ind1 , ind2 + 1 , s1, s2 , dp) + 1;
+        int del = solve(ind1 + 1 , ind2 , s1 , s2 , dp) + 1;
+        int rep = solve(ind1 + 1 , ind2 + 1 , s1  ,s2 , dp) + 1;
+
+        return dp[ind1][ind2] =  min(insert,min(del , rep));
+    }
+public:
+    int minDistance(string word1, string word2) {
+        int n = word1.length();
+        int m = word2.length();
+
+        vector<vector<int>>dp( n , vector<int>( m , -1));
+        return solve(0 , 0 , word1 , word2 ,dp );
+    }
+};
+
+
+/*
 Time Complexity = O(N*M)
 Space Complexity = O(N*M)
 Using Memoization
