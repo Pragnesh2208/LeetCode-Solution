6f201623d3d631036dacedb87bb6b990126617aa
Pragnesh Amadavadi <pragnesh.amadavadi07@gmail.com>
2024-09-05 08:07:17 +0530

    updated solution for construct binary tree from inorder and preorder

--- Binary Tree/#106 Construct a Binary Tree From Inorder and PostOrder Traversal.cpp
+++ Binary Tree/#106 Construct a Binary Tree From Inorder and PostOrder Traversal.cpp
@@ -9,8 +9,8 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-/*********************Optimal Approach for Construct a Binary Tree From Inorder and PostOrder Traversal***************/
-/*********************Time complexity = O (N)*************************************************************************/
+/*********************Better Approach for Construct a Binary Tree From Inorder and PostOrder Traversal***************/
+/*********************Time complexity = O (N * N)*************************************************************************/
 /**********************Space complexity = O (H)**********************************************************************/
 
 class Solution {
@@ -34,5 +34,29 @@ public:
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
         int ind = inorder.size() - 1;
       return  Build(inorder , postorder ,ind  , 0 , inorder.size() - 1);
+    }
+};
+
+/*********************Better Approach for Construct a Binary Tree From Inorder and PostOrder Traversal***************/
+/*********************Time complexity = O (N * LogN)*************************************************************************/
+/**********************Space complexity = O (H + N)**********************************************************************/
+class Solution {
+public:
+    TreeNode * cs(vector<int>&pr , map<int,int>&mp, int &ri , int low , int high) {
+        
+        if(low > high) return NULL;
+        TreeNode * root = new TreeNode(pr[ri]);
+        int pivot = mp[pr[ri]];
+        ri++;
+        root -> left = cs(pr , mp , ri  , low , pivot - 1);
+        root -> right = cs(pr , mp , ri  , pivot + 1 , high);
+
+        return root;
+    }
+    TreeNode* buildTree(vector<int>& pr, vector<int>& in) {
+        int rootIndex = 0;
+        map<int,int>mp;
+        for(int i = 0; i < in.size() ; i++) mp[in[i]] = i;
+        return cs(pr , mp , rootIndex , 0 , in.size() - 1);
     }
 };
