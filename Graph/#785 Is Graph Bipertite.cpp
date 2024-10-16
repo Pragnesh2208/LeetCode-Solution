8610f180e878e962fc9d9ab71e0b321492c855ee
Pragnesh Amadavadi <pragnesh.amadavadi07@gmail.com>
2024-10-16 22:30:55 +0530

    is Graph Bipertite updated

--- Graph/#785 Is Graph Bipertite.cpp
+++ Graph/#785 Is Graph Bipertite.cpp
@@ -29,3 +29,54 @@ public:
         return true;
     }
 };
+
+
+/**
+ * Optimised solution using dfs to check is Graph Bipertite
+ * Time complexity = O(M*N)
+ * Space Complexity = O(M*N)
+ */
+class Solution {
+    private:
+    bool dfs(int node , vector<vector<int>>&graph , vector<pair<bool,int>>&visited ) {  
+        queue<pair<int , int>>que;
+        que.push({node , 1});
+        visited[node] = make_pair(true ,1);
+        while(!que.empty()) {
+            int sz = que.size();
+            while(sz--) {
+                pair<int,int> frontNode = que.front();
+                que.pop();
+                int node = frontNode.first;
+                int nodeColor = frontNode.second;
+
+                for(int connectedNode : graph[node]) {
+                    pair<bool,int>connectedNodeInfo = visited[connectedNode];
+                    bool connectedNodeVisited = connectedNodeInfo.first;
+                    int connectedNodeColor = connectedNodeInfo.second;
+
+                    if(connectedNodeVisited) {
+                        if(connectedNodeColor == nodeColor) return false;
+                    } else {
+                        int currentColor =  nodeColor ==  1  ? 2 : 1;
+                        que.push({connectedNode , currentColor});
+                        visited[connectedNode] = {true , currentColor};
+                    }
+                }
+            }
+        }
+        
+
+        return true;
+    }
+public:
+    bool isBipartite(vector<vector<int>>& graph) {
+        int totalNodes = graph.size();
+        vector<pair<bool , int>>visited(totalNodes , make_pair(false , -1));
+
+        for(int node = 0 ; node < totalNodes ; node++) {
+            if(!visited[node].first && !dfs(node , graph , visited )) return false ;
+        }
+        return true;
+    }
+};
