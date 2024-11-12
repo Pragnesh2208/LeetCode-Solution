a60fb8893b73bd6c410c0601e410af520fe461e4
Pragnesh Amadavadi <pragnesh.amadavadi07@gmail.com>
2024-11-12 23:42:25 +0530

    Shortest path for directed acyclic graph

--- Graph/Algorithms/Shortest Path/#Shortest Path For Directed Acyclic Graph.cpp
+++ Graph/Algorithms/Shortest Path/#Shortest Path For Directed Acyclic Graph.cpp
@@ -3,40 +3,60 @@
 /****************************Time Complexity = O( V + E)*******************************/
 /***************************Space Complexity = O(V) as we are using visit array********/
 
-class solutions
-{
-private:
-    void toposort(int node vector<bool> &vis, stack<int> &st, vector<int> adj[])
-    {
-        vis[node] = true;
-        for (auto i : adj[node])
-        {
-            if (vis[i] == false)
-                toposort(i, vis , st);
+class Solution {
+    private:
+    void topoSort(vector<vector<pair<int , int>>>&graph, int vertex , vector<bool>&visit , stack<int>&st) {
+        if(visit[vertex]) return;
+        visit[vertex] = true;
+        for(pair<int,int> adj : graph[vertex]) {
+            int v = adj.first;
+            topoSort(graph , v , visit , st);
         }
-        st.push(node);
+        st.push(vertex);
     }
-public:
-    vector<int, int> sortestPath(int V, int src, vector<int, int> adj[])
-    {
-        vector<bool> vis(V, false);
-        vector<int, int> dist(V, INT_MAX);
-        dist[src] = 0;
-        stack<int> St;
-        toposort(src, vis, st, adj);
-
-        while (!st.empty())
-        {
+    
+  public:
+    vector<int> shortestPath(int V, int totalEdge, vector<vector<int>>& edges) {
+        // code here
+        vector<int>dist(V , 1e9);
+        vector<vector<pair<int , int>>>graph(V);
+        vector<bool>visit(V , false);
+        stack<int>st;
+        
+        for(int edge = 0 ; edge < totalEdge ; edge++ ) {
+            int source = edges[edge][0];
+            int dist = edges[edge][1];
+            int wt = edges[edge][2];
+            graph[source].push_back({dist , wt});
+        }
+        
+        for(int vertex = 0 ; vertex < V ; vertex++) {
+            if(!visit[vertex]) topoSort(graph , vertex , visit , st);
+        }
+        
+        while(!st.empty()) {
+            int node = st.top();
+            dist[node] = -1;
+            if(node == 0) break;
+            st.pop();
+        }
+        
+        dist[0] = 0;
+        
+        while(!st.empty()) {
             int node = st.top();
             st.pop();
-            if (dist[node] != INT_MAX)
-            {
-                for (auto i : node)
-                {
-                    if (dist[i] >= dist[i] + dist[node])
-                        dist[i] = dist[i] + dist[node];
+            
+            for(pair<int,int>adj : graph[node]) {
+                int v = adj.first;
+                int wt = adj.second;
+                
+                if(dist[node] + wt < dist[v]) {
+                    dist[v] = dist[node] + wt;
                 }
             }
         }
+        
+        return dist;
     }
 };
